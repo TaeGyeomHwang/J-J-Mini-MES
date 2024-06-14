97739bd8f1ad74c56d24159985392ac224b28c96
TaeGyeomHwang <jihuhw99@gmail.com>
2024-06-14 09:49:57 +0900

    Initial commit

---
+++ src/main/java/com/boot/mes6/Mes6Application.java
@@ -0,0 +1,13 @@
+package com.boot.mes6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mes6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mes6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/mes6/Mes6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.mes6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mes6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
