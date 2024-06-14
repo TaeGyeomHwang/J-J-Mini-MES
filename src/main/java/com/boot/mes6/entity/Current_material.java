9b8383723c29074d7efc0f43a9586da1b286cce3
YURI <oyr5329@gmail.com>
2024-06-14 10:38:58 +0900

    entity2

--- src/main/java/com/boot/mes6/entity/Current_material.java
+++ src/main/java/com/boot/mes6/entity/Current_material.java
@@ -1,9 +1,6 @@
 package com.boot.mes6.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +14,7 @@ public class Current_material {
 
     @Id
     @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long current_material_no;
 
     @Column(nullable = false)
--- src/main/java/com/boot/mes6/entity/Current_product.java
+++ src/main/java/com/boot/mes6/entity/Current_product.java
@@ -1,9 +1,6 @@
 package com.boot.mes6.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +12,7 @@ public class Current_product {
 
     @Id
     @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long current_product_no;
 
     @Column(nullable = false)
